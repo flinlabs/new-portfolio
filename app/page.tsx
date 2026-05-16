b6c384b693b7f4bb1de03ab3099c18dfff04ddf1
Felix Lin <f.lin@berkeley.edu>
2026-05-15 18:42:28 -0700

    added root layout with basic nav

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,6 @@ export default function Home() {
     <main>
       <h1>Faye Lin</h1>
       <p>Economics & Data Science @ UC Berkeley</p>
-      <nav>
-        <a href="/about">About</a>
-        <a href="/experience">Experience</a>
-        <a href="/projects">Projects</a>
-        <a href="/contact">Contact</a>
-      </nav>
     </main>
   )
 }
