3c5477aaa200d804e4c2aa250ccb28f8f91e7e09
Felix Lin <f.lin@berkeley.edu>
2026-05-15 18:58:22 -0700

    added contact page w basic form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,22 @@ export default function Contact() {
 	return (
 		<main>
 			<h1>Contact</h1>
-			<p>Coming soon.</p>
+			<p>Whether you have a role, a project, or just want to connect, I&apos;m always happy to chat.</p>
+			<form>
+				<div>
+					<label>Name</label>
+					<input type="text" name="name" placeholder="Your name" />
+				</div>
+				<div>
+					<label>Email</label>
+					<input type="email" name="email" placeholder="Your email" />
+				</div>
+				<div>
+					<label>Message</label>
+					<textarea name="message" placeholder="Your message" rows={5} />
+				</div>
+				<button type="submit">Send</button>
+			</form>
 		</main>
 	)
 }
